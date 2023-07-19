150ef872e89c017a1c082817b374b4bc2e8a9d27
Dima <dima.tsymbal12345@gmail.com>
2023-07-19 14:14:20 +0300

    update screen

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import * as THREE from 'three'
 import BoxGeometry from './BoxGeometry'
 
 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
 const scene: THREE.Scene = new THREE.Scene()
 const canvas = document.querySelector('.webgl') as HTMLBodyElement
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
@@ -12,10 +17,10 @@ const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
-    55,
+    75,
     window.innerWidth / window.innerHeight,
     0.1,
-    300
+    1000
 )
 camera.position.z = 1.5
 
@@ -33,6 +38,18 @@ const topLight = new THREE.PointLight(0xffffff, .5);
     topLight.shadow.camera.near = 5;
     topLight.shadow.camera.far = 400;
 
+    
+
+    window.addEventListener('resize', () => {
+        sizes.width = window.innerWidth
+        sizes.height = window.innerHeight
+    
+        camera.aspect = sizes.width / sizes.height
+        camera.updateProjectionMatrix()
+    
+        renderer.setSize(sizes.width, sizes.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    })
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
